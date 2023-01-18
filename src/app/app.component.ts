1c91e7da3ac5e6207d2053584ad3173138d13969
Xiangyu Liu <xiangyu.liu@mitre10.co.nz>
2023-01-18 14:26:24 +1300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'animations';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item: any) {
+    this.list.push(item);
+  }
+
+  onDelete(item: any) {}
 }
